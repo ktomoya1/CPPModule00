eaf20fe2fa21bfc52f7025340bbac73d18a09b82
Kudo Tomoya <ktomoya@c4r1s4.42tokyo.jp>
2024-07-23 11:46:09 +0900

    fixed megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,20 +2,19 @@
 
 void to_uppercase(std::string &str)
 {
-  for (std::string::size_type i = 0; i < str.size(); ++i) {
+  for (std::string::size_type i = 0; i < str.size(); ++i)
     str[i] = std::toupper(static_cast<unsigned char>(str[i]));
-  }
 }
 
 int main(int argc, char* argv[]) {
-  if (argc == 1) {
+  if (argc == 1)
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-  }
-
-  for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
-    to_uppercase(arg);
-    std::cout << arg;
+  else {
+    for (int i = 1; i < argc; ++i) {
+      std::string arg = argv[i];
+      to_uppercase(arg);
+      std::cout << arg;
+    }
   }
   std::cout << std::endl;
   return 0;
