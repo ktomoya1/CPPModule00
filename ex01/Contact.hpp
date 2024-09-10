4bf1a2b5b20665a726d77c835d11c782897e04a3
Kudo Tomoya <ktomoya@c3r5s3.42tokyo.jp>
2024-09-10 19:26:17 +0900

    Added destructor for Contact class

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -7,6 +7,9 @@ class Contact {
     : first_name(first_name), last_name(last_name), nickname(nickname), phone_number(phone_number), darkest_secret(darkest_secret) {
       std::cout << "Constructor called!" << std::endl;
     };
+    ~Contact() {
+      std::cout << "Destructor called!" << std::endl;
+    };
 
   private:
     std::string first_name;
