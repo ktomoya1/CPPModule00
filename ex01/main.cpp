eb1d8412251640878d1c114222fde9c6c29fbdbe
Kudo Tomoya <ktomoya@c3r6s2.42tokyo.jp>
2024-09-17 21:23:15 +0900

    fix: Added command input handling

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,5 +3,21 @@
 
 int main() {
   PhoneBook phonebook;
+
+  while (true) {
+    std::string command;
+
+    std::cout << "Please enter a command" << std::endl;
+    std::cin >> command;
+    if (command == "ADD") {
+      std::cout << "ADD" << std::endl;
+    } else if (command == "SEARCH") {
+      std::cout << "SEARCH" << std::endl;
+    } else if (command == "EXIT") {
+      return 0;
+    } else {
+      std::cout << "The valid commands are \"ADD\", \"SEARCH\" and \"EXIT\"" << std::endl;
+    }
+  }
   return 0;
 }
