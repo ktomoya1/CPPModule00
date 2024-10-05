7ee6bc1b5ece07520b59b519fdbb88eeebce05dc
tom <134240277+ktomoya1@users.noreply.github.com>
2024-10-06 05:09:13 +0900

    feat: Truncate field names longer than 10 characters

--- ex01/src/contact.cpp
+++ ex01/src/contact.cpp
@@ -12,26 +12,24 @@ std::string Contact::GetPhoneNumber() { return this->phone_number_; }
 
 std::string Contact::GetDarkestSecret() { return this->darkest_secret_; }
 
-void  Contact::SetIndex(const int index) {
-  this->index_ = index;
-}
+void Contact::SetIndex(const int index) { this->index_ = index; }
 
-void  Contact::SetFirstName(const std::string& first_name) {
+void Contact::SetFirstName(const std::string& first_name) {
   this->first_name_ = first_name;
 }
 
-void  Contact::SetLastName(const std::string& last_name) {
+void Contact::SetLastName(const std::string& last_name) {
   this->last_name_ = last_name;
 }
 
-void  Contact::SetNickname(const std::string& nickname) {
+void Contact::SetNickname(const std::string& nickname) {
   this->nickname_ = nickname;
 }
 
-void  Contact::SetPhoneNumber(const std::string& phone_number) {
+void Contact::SetPhoneNumber(const std::string& phone_number) {
   this->phone_number_ = phone_number;
 }
 
-void  Contact::SetDarkestSecret(const std::string& darkest_secret) {
+void Contact::SetDarkestSecret(const std::string& darkest_secret) {
   this->darkest_secret_ = darkest_secret;
 }
--- ex01/src/phonebook.cpp
+++ ex01/src/phonebook.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-
 #include "PhoneBook.h"
+
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 PhoneBook::PhoneBook() {
   for (int i = 0; i < kMaxContacts; ++i) {
@@ -42,7 +42,7 @@ void PhoneBook::ReceiveCommand() {
   return;
 }
 
-bool  PhoneBook::GetInput(const std::string& prompt, std::string& input) {
+bool PhoneBook::GetInput(const std::string& prompt, std::string& input) {
   std::cout << prompt;
   if (!std::getline(std::cin, input)) {
     if (std::cin.eof()) {
@@ -55,7 +55,7 @@ bool  PhoneBook::GetInput(const std::string& prompt, std::string& input) {
   return true;
 }
 
-bool  PhoneBook::GetInputNumber(const std::string& prompt, int& number) {
+bool PhoneBook::GetInputNumber(const std::string& prompt, int& number) {
   std::cout << prompt;
   int input_number;
   std::cin >> input_number;
@@ -70,7 +70,7 @@ bool  PhoneBook::GetInputNumber(const std::string& prompt, int& number) {
   return true;
 }
 
-bool  PhoneBook::OverwriteContact(Contact& contact) {
+bool PhoneBook::OverwriteContact(Contact& contact) {
   std::string first_name, last_name, nickname, phone_number, darkest_secret;
 
   if (!this->GetInput("Please enter the first name: ", first_name)) {
@@ -81,7 +81,8 @@ bool  PhoneBook::OverwriteContact(Contact& contact) {
     return false;
   } else if (!this->GetInput("Please enter the phone number: ", phone_number)) {
     return false;
-  } else if (!this->GetInput("Please enter the darkest secret: ", darkest_secret)) {
+  } else if (!this->GetInput("Please enter the darkest secret: ",
+                             darkest_secret)) {
     return false;
   };
   contact.SetFirstName(first_name);
@@ -92,7 +93,16 @@ bool  PhoneBook::OverwriteContact(Contact& contact) {
   return true;
 }
 
-void  PhoneBook::DisplayContactList(Contact* contacts, int size) {
+bool PhoneBook::SearchContact(Contact* contacts) {
+  int index;
+  if (!this->GetInputNumber(
+          "Please enter the index of the contact you want to view: ", index))
+    return false;
+  this->DisplayContactDetails(contacts[index]);
+  return true;
+}
+
+void PhoneBook::DisplayContactList(Contact* contacts, int size) {
   std::cout << "|     index|first name| last name|  nickname|" << std::endl;
   for (int i = 0; i < size; ++i) {
     this->DisplayContactRow(contacts[i]);
@@ -100,29 +110,37 @@ void  PhoneBook::DisplayContactList(Contact* contacts, int size) {
   return;
 }
 
-bool  PhoneBook::SearchContact(Contact* contacts) {
-  int index;
-  if (!this->GetInputNumber("Please enter the index of the contact you want to view: ", index))
-    return false;
-  this->DisplayContactDetails(contacts[index]);
-  return true;
-}
-
-void  PhoneBook::DisplayContactRow(Contact& contact) {
-  std::cout << "|" << std::setw(10) << contact.GetIndex()
-            << "|" << std::setw(10) << contact.GetFirstName()
-            << "|" << std::setw(10) << contact.GetLastName()
-            << "|" << std::setw(10) << contact.GetNickname()
-            << "|" << std::endl;
+void PhoneBook::DisplayContactRow(Contact& contact) {
+  std::string trunc_first_name = contact.GetFirstName(),
+              trunc_last_name = contact.GetLastName(),
+              trunc_nickname = contact.GetNickname();
+  if (trunc_first_name.size() > 10)
+    trunc_first_name = trunc_first_name.substr(0, 9) + ".";
+  if (trunc_last_name.size() > 10)
+    trunc_last_name = trunc_last_name.substr(0, 9) + ".";
+  if (trunc_nickname.size() > 10)
+    trunc_nickname = trunc_nickname.substr(0, 9) + ".";
+  std::cout << "|"
+            << std::setw(10) << contact.GetIndex() << "|"
+            << std::setw(10) << trunc_first_name   << "|"
+            << std::setw(10) << trunc_last_name    << "|"
+            << std::setw(10) << trunc_nickname     << "|"
+            << std::endl;
   return;
 }
 
-void  PhoneBook::DisplayContactDetails(Contact& contact) {
-  std::cout << std::setw(16) <<          "index: " << contact.GetIndex()         << std::endl
-            << std::setw(16) <<     "first name: " << contact.GetFirstName()     << std::endl
-            << std::setw(16) <<      "last name: " << contact.GetLastName()      << std::endl
-            << std::setw(16) <<       "nickname: " << contact.GetNickname()      << std::endl
-            << std::setw(16) <<   "phone number: " << contact.GetPhoneNumber()   << std::endl
-            << std::setw(16) << "darkest Secret: " << contact.GetDarkestSecret() << std::endl;
+void PhoneBook::DisplayContactDetails(Contact& contact) {
+  std::cout << std::setw(16) << "index: " << contact.GetIndex()
+            << std::endl
+            << std::setw(16) << "first name: " << contact.GetFirstName()
+            << std::endl
+            << std::setw(16) << "last name: " << contact.GetLastName()
+            << std::endl
+            << std::setw(16) << "nickname: " << contact.GetNickname()
+            << std::endl
+            << std::setw(16) << "phone number: " << contact.GetPhoneNumber()
+            << std::endl
+            << std::setw(16) << "darkest Secret: " << contact.GetDarkestSecret()
+            << std::endl;
   return;
 }
